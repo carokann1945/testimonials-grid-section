da8c86a78bb1ed720df504b52b8055f37d6fb0bb
carokann1945 <carokann1945@gmail.com>
2026-03-13 11:05:44 +0900

    chore: 접근성 수정. 이미지 alt를 data.name으로

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -19,7 +19,7 @@ export default function Card({ data }: CardProps) {
       {/* 이름 부분 */}
       <div className={cn('flex gap-[17px] items-center')}>
         <figure className={cn('w-7 h-7', 'rounded-full overflow-hidden')}>
-          <img src={data.img} alt={data.img} />
+          <img src={data.img} alt={data.name} />
         </figure>
         <div className={cn('flex flex-col gap-1')}>
           <p className={cn('typo-2', data.titleColor)}>{data.name}</p>
